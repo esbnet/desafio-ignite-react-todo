283a4199463ad526ed29b86eb32003ca465a9f32
esbnet <esbnet@gmail.com>
2023-07-26 12:48:10 -0300

    feat: :sparkles: create todo componente

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,25 @@
+import { BsTrash3Fill } from "react-icons/bs";
+import checkImg from "../assets/check.png";
+import UncheckImg from "../assets/uncheck.png";
+
+const isChecked = true;
+
 export function Todo() {
   return (
-    <>
-      <div className="">Todo</div>
-    </>
-  )
+    <div className="flex gap-3 justify-center items-center p-4 w-full h-16 text-gray-100 bg-gray-400 rounded-lg" >
+      <div>
+        {isChecked ? (
+          <img src={checkImg} />
+        ) : (
+          <img src={UncheckImg} />
+        )}
+      </div>
+      <div className="flex-1 text-sm text-gray-100">
+        <p>Ipsum dolor sit amet consectetur adipisicing elit j sdfç lasdf jkasdf jasdç fças fas jsad jfsf jlkasfj lkafj alskfsdal jkf</p>
+      </div>
+      <div>
+        <BsTrash3Fill/>
+      </div>
+    </div>
+  );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,12 @@
 import Clipboard from "../assets/Clipboard.png";
+import { Todo } from "./Todo";
+
+const todos = true;
 
 export function TodoList() {
   return (
     <>
-      <section className="mx-auto h-[287px] max-w-[736px] text-gray-300 mt-16">
+      <section className="mx-auto min-h-[287px] max-w-[736px] text-gray-300 mt-16">
         <div className="flex justify-between items-center h-5">
           <p className="flex gap-2 justify-center items-center text-sm font-bold text-blue">
             Tarefas criadas
@@ -18,14 +21,27 @@ export function TodoList() {
             </strong>
           </p>
         </div>
-        <div className="flex flex-col justify-center items-center mt-6 w-full min-w-full h-56 text-gray-300 rounded-lg border-t border-gray-400">
-          <img src={Clipboard} />
-          <div className="flex flex-col mt-4 h-11">
-            <strong className="text-base font-bold">
-              Você ainda não tem tarefas cadastradas
-            </strong>
-            <span>Crie tarefas e organize seus itens a fazer</span>
-          </div>
+        <div className="flex flex-col gap-3 items-center mt-6 w-full max-w-full h-56 text-gray-300 rounded-lg border-t border-gray-400">
+          {todos ? (
+            <>
+              <Todo />
+              <Todo />
+              <Todo />
+              <Todo />
+              <Todo />
+              <Todo />
+            </>
+          ) : (
+            <>
+              <img src={Clipboard} />
+              <div className="flex flex-col mt-4 h-11">
+                <strong className="text-base font-bold">
+                  Você ainda não tem tarefas cadastradas
+                </strong>
+                <span>Crie tarefas e organize seus itens a fazer</span>
+              </div>
+            </>
+          )}
         </div>
       </section>
       <section></section>
